9db081fa36e875e491901730b0628048168321c6
buildwithmohanad <buildwithmohanad@gmail.com>
2025-08-13 12:57:52 +0300

    seo imporvments and google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,46 @@ import Navbar from '@/components/navbar';
 import GSAPProvider from '../components/GSAPProvider';
 import Loading from './loading';
 import { Suspense } from 'react';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const metadata: Metadata = {
-  title: 'Spylt remake',
-  description: 'remake of an awwward winning website using Nextjs and GSAP by Mohannad',
+  title: 'Spylt remake | GSAP Nextjs website',
+  description: 'an awwward winning GSAP website remake by Mohannad a Front-end dev',
+  keywords: ['Mohannad Dev', 'awwward winning website remake', 'nextjs', 'react', 'GSAP'],
+  authors: [{ name: 'Mohannad' }],
+  creator: 'Mohannad',
+  publisher: 'Spylt',
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      'index': true,
+      'follow': true,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://mohannad-spylt.vercel.app/',
+    siteName: 'Spylt',
+    title: 'Spylt remake | GSAP Nextjs website',
+    description: 'an awwward winning GSAP website remake by Mohannad a Front-end dev',
+    images: [
+      {
+        url: '/assets/images/og-image.png', 
+        width: 1431,
+        height: 738,
+        alt: 'Spylt remake',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Spylt remake | GSAP Nextjs website',
+    description: 'an awwward winning GSAP website remake by Mohannad a Front-end dev',
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
@@ -23,6 +59,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             </Suspense>
           </div>
         </div>
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || ''} />
       </body>
     </html>
   );
