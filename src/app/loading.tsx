313c16fec9ebabd013cc13d55fffbd16ccdc4536
buildwithmohanad <buildwithmohanad@gmail.com>
2025-08-14 16:34:28 +0300

    fixing loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <section className="bg-red-brown text-milk inset-0 w-screen h-screen">
+    <section className="bg-red-brown text-milk w-screen h-screen">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-xl">Loading...</div>
     </section>
   );
